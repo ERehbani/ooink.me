5b6b700bbbf4138ab63af8fe63deebf53726a564
Elián Rehbani <elianrehbani@MacBook-Air-de-Emmanuel.local>
2023-10-04 15:32:11 -0300

    @vercel/analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@
 import Footer from 'src/components/footer/page'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Footer/>
+        <Analytics/>
       </body>
     </html>
   )
